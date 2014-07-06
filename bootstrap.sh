9668b96e9a396ad8966f138f15dee177cc2602e9
nirnanaaa <mosny@zyg.li>
2014-07-06 10:21:02 +0200

    changed postfix running user

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@
 [ -d /var/mail/vmail/sieve ] || mkdir -p /var/mail/vmail/sieve
 chown -R vmail:vmail /var/mail/vmail/sieve
 
+echo "$HOSTNAME" > /etc/hostname
 echo "$MAILNAME" > /etc/mailname
+postconf -e "myhostname=$HOSTNAME"
+
 echo "postmaster_address = postmaster@$MAILNAME" >> /etc/dovecot/conf.d/15-lda.conf
 
 PF_DIR=/etc/postfix DC_DIR=/etc/dovecot /template $PF_DB_USER $PF_DB_PASS $PF_DB_NAME $PF_DB_HOST
